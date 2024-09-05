50dadd8327d3b70db77ee65f82896d5a57427b89
asadbekumarov <asadbekumarov922@gmail.com>
2024-09-06 01:31:15 +0500

    end

---
+++ script.js
@@ -0,0 +1,117 @@
+// 1-masala
+
+// function yosh(a) {
+//   if (a > 18) {
+//     return "kattasiz";
+//   } else {
+//     return "kichkinasiz";
+//   }
+// }
+// console.log(yosh(5));
+
+// 2-masala
+
+// function son(a) {
+//   if (a >= 0) {
+//     return "musbat son";
+//   } else if (a < 0) {
+//     return "manfiy son";
+//   }
+// }
+// console.log(son(5));
+
+// 3-masala
+
+// function son(a, b, c) {
+//   let musbat = 0;
+//   if (a > 0) musbat++;
+//   if (b > 0) musbat++;
+//   if (c > 0) musbat++;
+//   return musbat;
+// }
+// console.log(son(2, -6, 9));
+
+// 4-masala
+
+// function son(a) {
+//   if (a % 2 === 0) {
+//     return "jub";
+//   } else {
+//     return "toq";
+//   }
+// }
+// console.log(son(1));
+
+// 5-masala
+
+// function son(a) {
+//   if (a % 2 !== 0) {
+//     return a * 2;
+//   } else if (a % 2 === 0)
+//      return a / 2;
+// }
+// console.log(son(6));
+
+// 6-masala
+
+// function son(a) {
+//   let num = 0;
+//   if (a === 0) {
+//     return 10;
+//   } else if (a > 0) {
+//     return a + 1;
+//   } else if (a < 0) {
+//     return a - 2;
+//   }
+// }
+
+// console.log(son(0));
+
+// 7-masala
+
+// function son(a, b) {
+//   if (a > b) {
+//     return [a, b];
+//   } else if (a < b) {
+//     return [b, a];
+//   }
+// }
+// console.log(son(55, 6));
+
+// 8-masala
+
+// function son(a) {
+//   if (a >= 0 && a % 2 === 0) {
+//     return a + 1;
+//   } else {
+//     return a;
+//   }
+// }
+
+// console.log(son(6));
+
+// 9-masala
+
+// function son(a, b) {
+//   if (a > b) {
+//     return a * 10;
+//   } else if (a < b) {
+//     return b * 10;
+//   }
+// }
+// console.log(son(5, 6));
+
+// 10-masala
+
+// function son(a, b, c) {
+//   let musbat = 0;
+//   let manfiy = 0;
+//   if (a > 0) musbat++;
+//   if (b > 0) musbat++;
+//   if (c > 0) musbat++;
+//   else if (a < 0) manfiy++;
+//   else if (b < 0) manfiy++;
+//   else if (c < 0) manfiy++;
+//   return { musbat, manfiy };
+// }
+// console.log(son(2, -6, 9));
